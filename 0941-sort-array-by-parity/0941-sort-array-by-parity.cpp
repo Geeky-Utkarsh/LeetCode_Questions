49b84e0ee09e6c2811d7f5eaa3cda9f20ff4f6cc
Sinister <48992400+27481@users.noreply.github.com>
2023-09-28 12:20:25 +0530

    Time: 7 ms (66.47%), Space: 16.5 MB (36.74%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        // sort(nums.begin(), nums.end(),[]{}{
-            // if()
-        // })
+        sort(nums.begin(), nums.end(), [](int a, int b){
+            if(a%2==0 && b%2!=0) // sorting even numbers before odd
+              return true;
+            else if(a%2!=0 && b%2==0) // sorting odd numbers after even 
+              return false;
+            else // for 2 even or odd number maintain their relative order 
+             return a<b;
+        });
 
-        vector<int>even;
-        vector<int>odd;
+        return nums;
 
-        for(auto e : nums){
-            if(e%2==0)
-              even.push_back(e);
-            else{
-                odd.push_back(e);
-            }
-        }
-        even.insert(even.end(),odd.begin(),odd.end());
-        return even;
+        // vector<int>even;
+        // vector<int>odd;
+
+        // for(auto e : nums){
+        //     if(e%2==0)
+        //       even.push_back(e);
+        //     else{
+        //         odd.push_back(e);
+        //     }
+        // }
+        // even.insert(even.end(),odd.begin(),odd.end());
+        // return even;
     }
 };
