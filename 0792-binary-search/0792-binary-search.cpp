e887dba97180c1d7248b37f752cf2e2c5cfd6f2e
Sinister <48992400+27481@users.noreply.github.com>
2023-10-13 11:44:33 +0530

    Time: 26 ms (73.17%), Space: 27.8 MB (40.88%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        auto it= lower_bound(nums.begin(), nums.end(), target);
-        int idx=distance(nums.begin(), it);
+        // auto it= lower_bound(nums.begin(), nums.end(), target);
+        // int idx=distance(nums.begin(), it);
 
-        if(it!=nums.end() && *it==target)
-          return idx;
-        else
-          return -1;
+        // if(it!=nums.end() && *it==target)
+        //   return idx;
+        // else
+        //   return -1;
+
+        int low=0;
+        int high=nums.size()-1;
+
+        while(low<=high){
+          int mid=(low+high)/2;
+
+          if(nums[mid]==target) return mid;
+          else if(nums[mid]<target) low=mid+1;
+          else if(nums[mid]>target) high=mid-1;
+          // else if (nums[mid] < target) low = mid + 1;  // Increase low
+          // else if (nums[mid] > target) high = mid - 1; // Decrease high
+        }
+        return -1;
     }
 };
