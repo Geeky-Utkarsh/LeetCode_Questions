f6127df1a9a528e64ed399c40aedcc5a1941e11d
Sinister <48992400+27481@users.noreply.github.com>
2023-10-14 18:35:52 +0530

    Time: 3 ms (91.96%), Space: 14.2 MB (12.34%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -13,20 +13,92 @@
 //         return res;
 //     }
 // };
-class Solution {
-public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> res = {-1, -1}; // Initialize the result with [-1, -1]
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         vector<int> res = {-1, -1}; // Initialize the result with [-1, -1]
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target) {
-                if (res[0] == -1) {
-                    res[0] = i; // Set the starting position
-                }
-                res[1] = i; // Update the ending position
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (nums[i] == target) {
+//                 if (res[0] == -1) {
+//                     res[0] = i; // Set the starting position
+//                 }
+//                 res[1] = i; // Update the ending position
+//             }
+//         }
+
+//         return res;
+//     }
+// };
+class Solution {
+    public:
+       vector<int>searchRange(vector<int>&nums, int target){
+        //  vector<int>res={-1,-1};
+
+        //  int n=nums.size();
+        //  int lo=0;
+        //  int hi=n-1;
+
+        //  // First Occurrence 
+        //  while(lo<=hi){
+        //     int mid=lo+(hi-lo)/2;
+            
+        //     if(nums[mid]==target){
+        //         if(mid==0){
+        //             res[0]=mid;
+        //         }
+        //         else if(nums[mid-1]!=target){
+        //             res[0]=mid;
+        //             break;
+        //         }
+        //         else{
+        //             hi=mid-1;
+        //         }
+        //     }
+        //     else if(nums[mid]>target)
+        //         hi=mid-1;
+        //     else if(nums[mid]<target)
+        //         lo=mid+1;
+        //  }
+
+         // Last Occurrence 
+        //   lo=0;
+        //   hi=n-1;
+
+        //  while(lo<=hi){
+        //      int mid=lo+(hi-lo)/2;
+
+        //      if(nums[mid]==target){
+        //          if(nums[mid+1]!=target){
+        //              res[1]=mid;
+        //              break;
+        //          }
+        //          else{
+        //              lo=mid+1;
+        //          }
+        //      }
+        //      else if(nums[mid]>target)
+        //           hi=mid-1;
+        //      else if(nums[mid]<target)
+        //           lo=mid+1;
+        //  }
+        // First Occurrence
+        vector<int>res={-1,-1};
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==target){
+                res[0]=i;
+                break;
             }
         }
 
-        return res;
-    }
+        // last occurence
+        for(int i=nums.size()-1; i>=0; i--){
+            if(nums[i]==target){
+               res[1]=i;
+               break;
+            }
+        }
+         return res;
+      }
 };
