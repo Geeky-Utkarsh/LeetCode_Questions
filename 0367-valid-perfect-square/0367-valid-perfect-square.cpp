56b21e6b541833d00bb6d31c881803ca1737414d
Sinister <48992400+27481@users.noreply.github.com>
2023-10-15 06:03:18 +0530

    Time: 0 ms (100%), Space: 6.2 MB (44.69%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        // for TC of O(1)
+
+        // int n=sqrt(num);
+        // if(n*n==num)
+        //    return true;
+        // else 
+        //    return false;
+
+        // for TCof O(log n)
+        int x=num;
+        int lo=0;
+        int hi=x;
+        
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            long long m=(long long)mid;
+            long long y=(long long)x;
+
+            if(m*m==y) return true;
+            else if(m*m>y) hi=mid-1;
+            else
+               lo=mid+1;
+        }
+        return false;
+    }
+};
