0b12444373546abc48323ace86bbf1ed19ab7f8b
Sinister <48992400+27481@users.noreply.github.com>
2023-10-15 13:35:18 +0530

    Time: 93 ms (19.99%), Space: 60 MB (22.91%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -1,12 +1,44 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int s=arr.size();
+        // int s=arr.size();
 
-        for(int i=0; i<=s-2; i++){
-            if(arr[i]>arr[i+1] && arr[i]>arr[i+1])
-               return i;
+        // for(int i=0; i<=s-2; i++){
+        //     if(arr[i]>arr[i+1] && arr[i]>arr[i+1])
+        //        return i;
+        // }
+        // return 0;
+
+    //     int s=arr.size();
+    //     int lo=0;
+    //     int hi=s-1;
+
+    //     while(lo<=hi){
+    //         int mid=lo+(hi-lo)/2;
+
+    //         if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1])
+    //            return mid;
+    //         else if(arr[mid]<arr[mid+1])
+    //            hi=mid-1;
+    //         else
+    //           lo=mid+1;
+    //     }
+    //     return 0;
+        int s=arr.size();
+        int lo=1;
+        int hi=s-2;
+
+        while (lo<=hi) {
+            int mid =lo+(hi-lo)/2;
+
+            if (arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]) {
+                return mid;
+            } else if(arr[mid]<arr[mid+1]) {
+                lo=mid+1;
+            } else {
+                hi = mid-1;
+            }
         }
-        return 0;
+        return -1; // Peak not found
     }
 };
