03aa42390e4dd9855f499e2d83748df5d9a09499
Sinister <48992400+27481@users.noreply.github.com>
2023-10-17 12:56:32 +0530

    Time: 0 ms (100%), Space: 7.4 MB (16.4%) - LeetHub

--- 2561-number-of-distinct-averages/2561-number-of-distinct-averages.cpp
+++ 2561-number-of-distinct-averages/2561-number-of-distinct-averages.cpp
@@ -1,34 +1,48 @@
-// class Solution {
-// public:
-//     int distinctAverages(vector<int>& nums) {
-
-//         set<int>unique;
-//         sort(nums.begin(), nums.end());
-        
-//         // Using BruteForce approach O(n)
-//         for(int i=0; i<nums.size(); i++){
-//             for(int j=i+1; j<nums.size(); j++){
-//                 int avg=(nums[i]+nums[j])/2;
-//                 unique.insert(avg);
-//             }
-//         }
-//         return unique.size();
-//     }
-// };
 class Solution {
 public:
     int distinctAverages(vector<int>& nums) {
-        set<double> uniqueAverages;
-        int n = nums.size();
 
-        while (nums.size() >= 2) {
-            int minElem = *min_element(nums.begin(), nums.end());
-            int maxElem = *max_element(nums.begin(), nums.end());
-            uniqueAverages.insert((minElem + maxElem) / 2.0);
-            nums.erase(find(nums.begin(), nums.end(), minElem));
-            nums.erase(find(nums.begin(), nums.end(), maxElem));
+        // set<double>unique;
+        // for(int i=0; i<nums.size()-2; i++){
+
+        //     auto max=max_element(nums.begin(), nums.end());
+        //     auto min=min_element(nums.begin(), nums.end());
+
+        //     nums.erase(max);
+        //     nums.erase(min);
+                
+        //     int avg=(*max+*min)/2;
+        //     unique.insert(avg);
+        // }
+        set<double>unique;
+        int i=0,j=nums.size()-1;
+
+        sort(nums.begin(), nums.end());
+
+        while(i<j){
+            unique.insert(double(nums[i]+nums[j])/2);
+            i++;
+            j--;
         }
-
-        return uniqueAverages.size();
+        return unique.size();
     }
-};
+};  
+
+
+// class Solution {
+// public:
+//     int distinctAverages(vector<int>& nums) {
+//         set<double> uniqueAverages;
+//         int n = nums.size();
+
+//         while (nums.size() >= 2) {
+//             int minElem = *min_element(nums.begin(), nums.end());
+//             int maxElem = *max_element(nums.begin(), nums.end());
+//             uniqueAverages.insert((minElem + maxElem) / 2.0);
+//             nums.erase(find(nums.begin(), nums.end(), minElem));
+//             nums.erase(find(nums.begin(), nums.end(), maxElem));
+//         }
+
+//         return uniqueAverages.size();
+//     }
+// };
