e669fb383ab8886f4ec36382430431a073242548
Sinister <48992400+27481@users.noreply.github.com>
2023-10-27 01:22:07 +0530

    Update 0344-reverse-string.cpp
    i have added all kind 3 approaches used in this coding question of reversing string

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -32,8 +32,26 @@ public:
     //        // Recursive call on the substring excluding the swapped characters
     //        revFun(s,left+1,right-1);
     //   }
-
-
-
     }
 };
+
+
+
+1. Two-Pointer Approach:
+Time Complexity: O(n) - Linear time complexity. It iterates through the vector once, where n is the number of elements in the vector.
+Space Complexity: O(1) - Constant space complexity. It doesn't require additional memory allocation beyond a few variables.
+    
+2. Temporary Copy and Swap Approach:
+Time Complexity: O(n) - Linear time complexity. It iterates through the vector once to copy it and then swaps the elements in reverse order.
+Space Complexity: O(n) - Linear space complexity. It creates a copy of the original vector, consuming additional memory equal to the size of the vector.
+    
+3. Recursive Approach:
+Time Complexity: O(n) - Linear time complexity. It involves multiple recursive calls, each handling a portion of the vector. In total, it's still linear.
+Space Complexity: O(n) - Linear space complexity. Each recursive call adds a new frame to the call stack, and the space required for the call stack can be up to the size of the vector.
+
+    
+In terms of time complexity, all three approaches have a similar and efficient O(n) time complexity, 
+where n is the number of elements in the vector. 
+However,they differ in space complexity, 
+with the two-pointer approach being the most memory-efficient, as it has O(1) space complexity. 
+The other two approaches have O(n) space complexity because they involve additional memory allocation.
