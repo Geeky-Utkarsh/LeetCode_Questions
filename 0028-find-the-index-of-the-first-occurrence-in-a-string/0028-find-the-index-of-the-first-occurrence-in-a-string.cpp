253ca123bf935aa659d675cc29d0c8d842fbc9d3
Sinister <48992400+27481@users.noreply.github.com>
2023-10-29 13:37:55 +0530

    Time: 0 ms (100%), Space: 6.7 MB (35.3%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+    //    string n=find(needle);
+    // }
+    // if((n.size())>0)
+    //    return -1;
+    // else 
+    //  return n;
+
+    // int fA=0;
+
+    // if(haystack.find(needle)!=string::npos){
+    //     fA=haystack.find(needle);
+    //     return 0;
+    //  }
+    // return -1;
+    return haystack.find(needle);
+ 
+    }
+};
