b29e599012398ec13c4ff3bafe17c069d1e99626
Sinister <48992400+27481@users.noreply.github.com>
2024-01-18 05:10:12 +0530

    Time: 6 ms (17.69%) | Memory: 8.7 MB (20.56%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,35 +1,56 @@
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
 class Solution {
 public:
-    bool uniqueOccurrences(vector<int>& arr) {
-
-        unordered_map<int,int>mp;
-
-        for(auto e: arr){
-            mp[e]++;
+    bool uniqueOccurrences(std::vector<int>& arr) {
+        std::unordered_map<int, int> map;
+        for (int num : arr) {
+            map[num] = map[num] + 1;
         }
 
-
-        // Checking for duplicate occurrence counts
-        unordered_map<int,int>Unique_Occurrence;
-
-        for(auto pair : mp){
-            // If an occurrence count is already in the map, it's a duplicate
-            if(Unique_Occurrence.find(pair.second)!=Unique_Occurrence.end())
-              return false;
-            
-            Unique_Occurrence[pair.second]=1;  // Mark the count as unique 
+        std::unordered_set<int> set;
+        for (const auto& pair : map) {
+            set.insert(pair.second);
         }
-        return true;
 
-
-
-        // set<int>stt;
-        // for(auto e : duplicates){
-        //     stt.insert(e);
-        // }
-        // int s=stt.size();
-        // int v=arr.size();
-
-        // return s==v;
+        return set.size() == map.size();
     }
 };
+
+// class Solution {
+// public:
+//     bool uniqueOccurrences(vector<int>& arr) {
+
+//         unordered_map<int,int>mp;
+
+//         for(auto e: arr){
+//             mp[e]++;
+//         }
+
+
+//         // Checking for duplicate occurrence counts
+//         unordered_map<int,int>Unique_Occurrence;
+
+//         for(auto pair : mp){
+//             // If an occurrence count is already in the map, it's a duplicate
+//             if(Unique_Occurrence.find(pair.second)!=Unique_Occurrence.end())
+//               return false;
+            
+//             Unique_Occurrence[pair.second]=1;  // Mark the count as unique 
+//         }
+//         return true;
+
+
+
+//         // set<int>stt;
+//         // for(auto e : duplicates){
+//         //     stt.insert(e);
+//         // }
+//         // int s=stt.size();
+//         // int v=arr.size();
+
+//         // return s==v;
+//     }
+// };
