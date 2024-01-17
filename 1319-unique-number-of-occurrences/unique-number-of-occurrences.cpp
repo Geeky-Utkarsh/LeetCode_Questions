fba2b869704339ade212f4700c7fdf5c7ec5d5a0
Sinister <48992400+27481@users.noreply.github.com>
2024-01-18 05:12:40 +0530

    Time: 5 ms (39.16%) | Memory: 8.9 MB (9.56%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -2,45 +2,45 @@
 #include <unordered_set>
 #include <vector>
 
-class Solution {
-public:
-    bool uniqueOccurrences(std::vector<int>& arr) {
-        std::unordered_map<int, int> map;
-        for (int num : arr) {
-            map[num] = map[num] + 1;
-        }
-
-        std::unordered_set<int> set;
-        for (const auto& pair : map) {
-            set.insert(pair.second);
-        }
-
-        return set.size() == map.size();
-    }
-};
-
 // class Solution {
 // public:
-//     bool uniqueOccurrences(vector<int>& arr) {
-
-//         unordered_map<int,int>mp;
-
-//         for(auto e: arr){
-//             mp[e]++;
+//     bool uniqueOccurrences(std::vector<int>& arr) {
+//         std::unordered_map<int, int> map;
+//         for (int num : arr) {
+//             map[num] = map[num] + 1;
 //         }
 
+//         std::unordered_set<int> set;
+//         for (const auto& pair : map) {
+//             set.insert(pair.second);
+//         }
 
-//         // Checking for duplicate occurrence counts
-//         unordered_map<int,int>Unique_Occurrence;
+//         return set.size() == map.size();
+//     }
+// };
 
-//         for(auto pair : mp){
-//             // If an occurrence count is already in the map, it's a duplicate
-//             if(Unique_Occurrence.find(pair.second)!=Unique_Occurrence.end())
-//               return false;
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+
+        unordered_map<int,int>mp;
+
+        for(auto e: arr){
+            mp[e]++;
+        }
+
+
+        // Checking for duplicate occurrence counts
+        unordered_map<int,int>Unique_Occurrence;
+
+        for(auto pair : mp){
+            // If an occurrence count is already in the map, it's a duplicate
+            if(Unique_Occurrence.find(pair.second)!=Unique_Occurrence.end())
+              return false;
             
-//             Unique_Occurrence[pair.second]=1;  // Mark the count as unique 
-//         }
-//         return true;
+            Unique_Occurrence[pair.second]=1;  // Mark the count as unique 
+        }
+        return true;
 
 
 
@@ -52,5 +52,5 @@ public:
 //         // int v=arr.size();
 
 //         // return s==v;
-//     }
-// };
+    }
+};
