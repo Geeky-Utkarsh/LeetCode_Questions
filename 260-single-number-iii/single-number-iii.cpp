a22f183513bcd3a1d55da45a8642104ab963e62c
Sinister <48992400+27481@users.noreply.github.com>
2024-02-25 18:03:20 +0000

    Time: 3 ms (97.82%) | Memory: 13.9 MB (16.65%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -4,20 +4,33 @@ public:
         int n=nums.size();
         vector<int>arr;
 
-        unordered_map<int,int>mp;
-        auto it=nums.begin();
+    //     unordered_map<int,int>mp;
+    //     auto it=nums.begin();
 
-        for(it; it!=nums.end(); it++){
-            mp[*it]++;
+    //     for(it; it!=nums.end(); it++){
+    //         mp[*it]++;
+    //     }
+    //     for(auto &e : mp){
+    //         if(e.second==1)
+    //           arr.push_back(e.first);
+    //         if(arr.size()==2)
+    //           return arr;
+    //     }
+    //     return arr;
+
+    // 2nd approach 
+
+    unordered_set<int>uniqueEle;
+
+    for(auto &e : nums){
+    // If the element is already in the set, remove it
+        if(uniqueEle.find(e)!=uniqueEle.end()) {
+                uniqueEle.erase(e);
         }
-
-        for(auto &e : mp){
-            if(e.second==1)
-              arr.push_back(e.first);
-
-            if(arr.size()==2)
-              return arr;
-        }
-        return arr;
+        else { // Otherwise, add it to the set
+                uniqueEle.insert(e);
+            }
+     }
+     return vector<int>(uniqueEle.begin(), uniqueEle.end());
     }
 };
