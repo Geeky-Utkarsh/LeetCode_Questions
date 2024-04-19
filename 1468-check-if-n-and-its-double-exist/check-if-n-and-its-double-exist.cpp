ec0b50bae993b36ee339b4f30ad3e438b973bcee
Sinister <48992400+27481@users.noreply.github.com>
2024-04-19 23:09:58 +0530

    Time: 4 ms (76.66%) | Memory: 14.9 MB (5.06%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -1,12 +1,40 @@
+// Brute Force n^2 approach 
+
+// class Solution {
+// public:
+//     bool checkIfExist(vector<int>& arr) {
+//         for(int i=0; i<arr.size(); ++i){
+//             for(int j=0; j<arr.size(); ++j){
+//                 if(i!=j){
+//                     if(arr[i]==2*arr[j])
+//                         return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };                                       TC=> O(n^2)  SC=>(1)
+
+
+// Optimised Approach 
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        for(int i=0; i<arr.size(); ++i){
-            for(int j=0; j<arr.size(); ++j){
-                if(i!=j){
-                    if(arr[i]==2*arr[j])
-                        return true;
-                }
+        // we will be using hash map 
+        // We have to check 2 conditions 
+            //  i!=j
+            //  arr[i]==2*arr[j]
+
+        unordered_map<int,int>mp;
+
+        for(int i=0; i<arr.size(); i++){
+            mp.insert({arr[i],i});
+            // mp[arr[i]]==i;
+        }   
+
+        for(int i=0; i<arr.size(); i++){
+            if(mp.find(arr[i]*2)!=mp.end() && mp[arr[i]*2]!=i){
+                return true;
             }
         }
         return false;
