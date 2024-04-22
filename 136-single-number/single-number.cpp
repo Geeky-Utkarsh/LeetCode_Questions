b991745782ad208533f53bdf847626d98ca8b105
Sinister <48992400+27481@users.noreply.github.com>
2024-04-22 20:41:36 +0530

    Time: 7 ms (96.53%) | Memory: 19.2 MB (85.39%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int>m;
+        // unordered_map<int,int>m;
 
-        for(auto e : nums)
-          m[e]++;
+        // for(auto e : nums)
+        //   m[e]++;
 
-        for(auto pair : m){
-            if(pair.second==1)
-              return pair.first;
+        // for(auto pair : m){
+        //     if(pair.second==1)
+        //       return pair.first;
+        // }
+        // return 0;
+
+        // Using XOR as it is associative  
+        int res=0;
+        for(auto &ch : nums){
+            res^=ch;
         }
-        return 0;
+        return res;
     }
 };
