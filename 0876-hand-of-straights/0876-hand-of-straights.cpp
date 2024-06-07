3bc7d1b748001e6d98ff47a0cd315663083e9d26
Sinister <48992400+27481@users.noreply.github.com>
2024-06-07 12:24:02 +0530

    Time: 61 ms (53.2%), Space: 30.7 MB (42.24%) - LeetHub

---
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+
+        if(hand.size()%groupSize!=0)  // edge case -> if size of hand array is not divible by int groupSize then false 
+            return false;
+
+        map<int,int>mp;
+
+        // maping the hands array elements 
+        for(auto &e : hand){
+            mp[e]++;
+        }
+
+        while(!mp.empty()){
+
+            int curr=mp.begin()->first; 
+
+            for(int i=0; i<groupSize; i++){
+                if(mp[curr+i]==0){
+                    return false;
+                }
+
+                mp[curr+i]--;
+                if(mp[curr+i]<1){
+                    mp.erase(curr+i);
+                }
+            }
+        }
+        return true;
+
+    }
+};
