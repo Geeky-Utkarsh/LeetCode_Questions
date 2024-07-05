e058f6bd2976791da6f7ed15fd6190c046630f62
Sinister <48992400+27481@users.noreply.github.com>
2024-07-05 10:56:16 +0530

    Time: 154 ms (89.14%), Space: 115.7 MB (95.45%) - LeetHub

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,6 +1,94 @@
+// class Solution {
+// public:
+//     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+//         int idx=1;
+//         int firstIdx=-1;
+//         int lastIdx=-1;
+
+//         ListNode*a=head;
+//         ListNode*b=head->next;
+//         ListNode*c=head->next;
+
+//         if(c==NULL) return {-1,-1};
+
+//         int minD=INT_MAX;
+//         int f=-1;
+//         int s=-1;
+
+//         while(c!=NULL){
+//            if((b->val > a->val && b->val > c->val) || (b->val < a->val && b->val < c->val)) {
+
+//             // maxd
+//             if (firstIdx==-1)
+//                     firstIdx=idx;
+//            else 
+//              lastIdx=idx;
+
+//         // mind 
+//         f=s;
+//         s=idx;
+
+//         if (f!=-1) {
+//             int d=s-f;
+//             minD=min(minD,d);
+//         }
+//     }
+//     a = a->next;
+//     b = b->next;
+//     c = c->next;
+//     idx++;
+// }
+        
+//     if(lastIdx==-1) return {-1,-1};
+//     int maxD=lastIdx-firstIdx;
+
+//     return {minD, maxD};
+//     }
+// };
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        
+        if (head == nullptr || head->next == nullptr || head->next->next == nullptr)
+            return {-1, -1};
+
+        int idx=1;
+        int firstIdx=-1;
+        int lastIdx=-1;
+
+        ListNode* a=head;
+        ListNode* b=head->next;
+        ListNode* c=head->next->next;
+
+        int minD=INT_MAX;
+        int f=-1;
+        int s=-1;
+
+        while (c != nullptr) {
+            if ((b->val > a->val && b->val > c->val) || (b->val < a->val && b->val < c->val)) {
+                if (firstIdx==-1)
+                    firstIdx=idx;
+                else
+                    lastIdx=idx;
+
+                f=s;
+                s=idx;
+
+                if (f!=-1) {
+                    int d=s-f;
+                    minD=min(minD, d);
+                }
+            }
+            a = a->next;
+            b = b->next;
+            c = c->next;
+            idx++;
+        }
+
+        if (lastIdx==-1)
+            return {-1,-1};
+
+        int maxD=lastIdx-firstIdx;
+
+        return {minD,maxD};
     }
 };
