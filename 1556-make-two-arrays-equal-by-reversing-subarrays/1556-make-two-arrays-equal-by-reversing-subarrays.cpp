34d494f65d54c9fd7c54545da39bc34dbca2e13e
Sinister <48992400+27481@users.noreply.github.com>
2024-08-03 18:58:42 +0530

    Time: 13 ms (29.25%), Space: 18.1 MB (30.59%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,18 +1,36 @@
-class Solution {
+class Solution1{
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-    //     int f1[target.size()+1]={0};
-    //     int f2[target.size()+1]={0};
-
-    //     // mapping the elements of the array arr into frequency array f1
-    //     for(auto &e : arr){
-    //         f1[e]++;
-    //     }
         sort(target.begin(), target.end());
         sort(arr.begin(), arr.end());
 
         for(int i=0; i<arr.size(); i++){
             if(arr[i]!=target[i])
+               return false;
+        }
+        return true;
+    }
+};
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+class Solution{
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        // using Frequnecy array 
+        int f[1001]={0};
+
+        // mapping element into frequency array 
+        for(int &e : arr){
+            f[e]++;
+        }
+        // now checking the frequency aray f for each element 
+        for(int &e : target){
+            f[e]--;
+        }
+
+        for(int &e : f){
+            if(e!=0)
                return false;
         }
         return true;
