12aa02f1ba8a1f85c41058d5088eeb7845bbbecf
Sinister <48992400+27481@users.noreply.github.com>
2024-08-18 02:02:32 +0530

    Time: 15 ms (100%), Space: 33.3 MB (21.43%) - LeetHub

---
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int>resultsArray(vector<int>& nums, int k){
+    int n=nums.size();
+    vector<int>results;
+
+    for (int i=0; i<=n-k; ++i) {
+        bool isValid = true;
+
+        // Check if the current window is consecutive and sorted
+        for (int j=i+1; j <i+k; ++j) {
+            if (nums[j]!=nums[j-1]+1){
+                isValid=false;
+                break;
+            }
+        }
+
+        if (isValid){
+            results.push_back(nums[i+k-1]);
+        } else {
+            results.push_back(-1);
+        }
+    }
+    return results;
+    }
+};
