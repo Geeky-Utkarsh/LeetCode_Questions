19a844f2dcf3300424ef83ce85fdf7d1eb7cc8ec
Sinister <48992400+27481@users.noreply.github.com>
2024-08-25 14:33:57 +0530

    Time: 13 ms (30%), Space: 26 MB (90%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -7,8 +7,9 @@ public:
         //    int val=*min(nums.begin(), nums.end());
         //    int idx_min=find(nums.begin(), nums.end(), val);
         //    nums[idx_min]=val*multipler;
-        auto min_it=min_element(nums.begin(), nums.end());
-         *min_it=*min_it*multiplier;
+
+        auto min_it=min_element(nums.begin(), nums.end()); // it will return an iterator pointing to the index of the minimum element
+         *min_it=*min_it*multiplier;// multiplying that nums index element with val
 
         }
         return nums;
