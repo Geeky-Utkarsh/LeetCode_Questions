112f62b4d1819398dad9fcf520d344ed40fb9716
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2024-09-23 16:02:31 +0530

    Time: 12 ms (59.18%), Space: 16 MB (8.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // Brute Force O(n^2)
@@ -10,6 +10,28 @@ public:
                 //   res.push_back(nums[i],nums[j]);
                 return {i,j};
             }
+        }
+        return {};
+    }
+};
+class Solution{
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>f;
+
+        for(int i=0; i<nums.size(); i++){
+            f[nums[i]]=i;
+        }     
+
+        for(int i=0; i<nums.size(); i++){
+            int val=nums[i];
+            int to_find=target-val;
+
+            // Checking if the compliment exits in the map or not and also that its not the same index
+            if(f.find(to_find)!=f.end() && f[to_find]!=i){
+                return {f[to_find],i};
+            }     
+            // f[val]=i;       
         }
         return {};
     }
