bffe948dc829909cc4b7594370f347f0034b59e6
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2024-09-29 19:40:36 +0530

    Time: 291 ms (75.27%), Space: 133.2 MB (65.86%) - LeetHub

--- 2388-replace-elements-in-an-array/2388-replace-elements-in-an-array.cpp
+++ 2388-replace-elements-in-an-array/2388-replace-elements-in-an-array.cpp
@@ -33,13 +33,15 @@ public:
             f[nums[i]]=i; // mapping the element of num array with there position i 
         }
 
-        // now replace the values in the nums by using map z 
+        // now replace the values in the nums by using map 
+        
         for(int i=0; i<o.size(); i++){
-            int k=f[o[i][0]];
-            f.erase(o[i][0]);
+            int k=f[o[i][0]]; // Find the index of the element to be replaced
+            f.erase(o[i][0]); // Remove the old element from the map
 
-            nums[k]=o[i][1];
-            f[o[i][1]]=k;
+            nums[k]=o[i][1];  // Replace the element in nums with the new value
+            f[o[i][1]]=k;     // Update the map with the new value and its position
+
         }
         return nums;
     }
