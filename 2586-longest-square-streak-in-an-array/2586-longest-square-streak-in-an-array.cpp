878978eb1cdc1700587c47cc27e51f68681dd21f
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2024-10-28 14:45:50 +0530

    Time: 87 ms (66.67%), Space: 109 MB (63.34%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int longestSquareStreak(vector<int>& nums) {
         // sorting + hash map based approach 
@@ -22,5 +22,34 @@ public:
         }
 
         return m_streak<2 ? -1 : m_streak;
+    }
+};
+
+class Solution{
+public:
+    int longestSquareStreak(vector<int>& nums) {
+        // unordered_set + hash map based approach 
+
+        unordered_set<int>look_up(nums.begin(), nums.end());
+        unordered_map<int,int>mp;
+
+        int m_streak=0;
+
+        for(int num : nums){
+            int streak=0;
+
+            long long int curr=num;
+
+            while(look_up.find(curr)!=look_up.end()){
+                streak+=1;
+                
+                curr=curr*curr;
+                if(curr>1e5)
+                    break;
+            }
+            m_streak=max(m_streak, streak);
+        }
+        return m_streak<2 ? -1 :  m_streak;
+
     }
 };
