1c0f9919fc38dd125586c6c33ec6e7c4ad84a649
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2024-12-07 21:18:35 +0530

    Time: 114 ms (33.12%), Space: 69.5 MB (36.62%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int maxOperations(vector<int>& nums, int k) {
         // we can have multiple approaches for this problem 
@@ -25,6 +25,30 @@ public:
                 j--;
             }
         } 
+        return cnt;
+    }
+};
+
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        // we will be using hashmap+compliement approach 
+
+        unordered_map<int,int>mp;
+        int cnt=0;
+
+        for( int num : nums){
+
+            int compliment=k-num;
+
+            if( mp.find(compliment)!=mp.end() && mp[compliment]>0 ){
+                cnt++;
+                mp[compliment]--; // we have to decrement the freqeuncy of the compliment , otherwise we will use that same number again ->twice
+            }
+            else{
+                mp[num]++;
+            }
+        }
         return cnt;
     }
 };
