c8f1b5eefe833fe3e3a8448946da7d4b5368a1c8
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2024-12-18 22:32:13 +0530

    Time: 0 ms (100%), Space: 14.2 MB (17.75%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1{
 public:
     vector<int> finalPrices(vector<int>& prices) {
 
@@ -27,6 +27,37 @@ public:
         }
         answer.pop_back();
         answer.push_back(prices[n-1]);
+        
+        return answer;
+    }
+};
+
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+
+        int n=prices.size();
+        
+        vector<int>answer(n);
+
+        int i=0;
+
+        while(i<n){
+            // int curr_price=prices[i];
+            // Finding the discount for curr_price
+            int j=i+1;
+            int discount=0;
+
+            while(j<n){
+                if(prices[j]<=prices[i]){
+                    discount=prices[j];
+                    break;
+                }
+                j++;
+            }
+            answer[i]=prices[i]-discount;
+            i++;
+        }
         
         return answer;
     }
