3222fa9542ca05ca3d920fa39fa9c65c9ba041e2
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2024-12-21 01:14:18 +0530

    Time: 0 ms (100%), Space: 11 MB (35.39%) - LeetHub

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,3 +1,25 @@
+class Solution1 {
+public:
+    int specialArray(vector<int>& nums) {
+
+        int n = nums.size();
+
+        for (int x=1; x<=n; ++x) {
+            int count=0;
+            for (int num : nums) {
+                if (num>=x) {
+                    count++;
+                }
+            }
+            if (count == x) {
+                return x;
+            }
+        }
+        
+        return -1;
+    }
+};
+
 class Solution {
 public:
     int specialArray(vector<int>& nums) {
