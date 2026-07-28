e076a4b92d063280dd0a1133b933ba1d600ef4ee
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2026-07-28 17:12:30 +0530

    Time: 0 ms (100%), Space: 13.5 MB (60.66%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -4,13 +4,13 @@ public:
         int f_max=0, s_max=0;
 
         for(int &e : nums){
-            if(e>f_max){
+            if(e>f_max){ // if [current element>f_max] then f_max becomes current element , s_max becomes previous s_max 
                 s_max=f_max;
                 f_max=e;
             }
-            else if(e>s_max)
+            else if(e>s_max) // if [current element > s_max] then s_max becomes -->current element 
               s_max=e;
         }
-        return (f_max-1)*(s_max-1);
+        return (f_max-1)*(s_max-1);  // this was asked in the question , to return multiplication of [f_max-1*s_max-1]
     }
 };
