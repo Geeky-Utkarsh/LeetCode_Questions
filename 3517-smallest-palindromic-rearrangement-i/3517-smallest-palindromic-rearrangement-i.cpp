7cf1db109185334b9b351acab203e36c3a33ecb2
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2026-07-28 16:18:39 +0530

    Time: 63 ms (44.4%), Space: 54 MB (98.46%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        if(s.size()==1 || s.size()==2)
+          return s;
+
+        int n=s.length();
+        int mid=n/2;
+
+        // Sorting the 1st half 
+        sort(s.begin(), s.begin()+mid);
+
+        // mirroring the 2nd half 
+        for(int i=0; i<mid; i++){
+            s[n-i-1]=s[i];
+        }
+        return s;
+
+    }
+};
